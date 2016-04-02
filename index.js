8844a0442ea08a5ff46f2d20591d5f2c5af50428
TerAleS <www.terales.info>
2016-04-02 14:58:52 +0300

    :construction: Set base url on object init

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 'use strict';
 
+import request from 'request';
+import endpointsSpec from './endpointsSpec.js';
+
 class nodeGlvrd {
 
   constructor(appName) {
     if (typeof appName !== 'string' || !appName.length) {
-      throw new TypeError(`appName is required and should be a string. Got ${typeof appName} ${appName}`);
+      throw new Error(`appName is required and should be a string. Got ${typeof appName} ${appName}`);
     }
 
     this.appName = appName;
+
+    this.request = request.defaults({
+      baseUrl: endpointsSpec.baseUrl,
+      timeout: 1000
+    });
   }
 }
 
--- test.js
+++ test.js
@@ -2,11 +2,14 @@
 
 import test from 'ava';
 import nodeGlvrd from './index';
+import endpointsSpec from './endpointsSpec.js';
 
 test.beforeEach(t => {
   t.context.glvrd = new nodeGlvrd('testApp');
 });
 
+test('should throw on empty appName', t => t.throws(() => { var t = new nodeGlvrd(); }, Error));
+
 test('should save appName', t => t.is(t.context.glvrd.appName, 'testApp'));
 
-test('should throw on empty input', t => t.throws(() => { nodeGlvrd(); }, TypeError));
+test('should set default request url', t => t.is(t.context.glvrd.request.defaults.baseUrl, endpointsSpec.baseUrl));
