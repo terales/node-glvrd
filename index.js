ba7031b6a8c29b52ad8494452155f4f9a90bc4a6
TerAleS <www.terales.info>
2016-04-02 10:23:14 +0300

    :construction: :white_check_mark: Create object with appName property

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-export default function nodeGlvrd(input) {
-  if (typeof input !== 'string') {
-    throw new TypeError(`\input\` should be \`String\`, got \`${typeof input}\``);
+'use strict';
+
+class nodeGlvrd {
+
+  constructor(appName) {
+    if (typeof appName !== 'string' || !appName.length) {
+      throw new TypeError(`appName is required and should be a string. Got ${typeof appName} ${appName}`);
+    }
+
+    this.appName = appName;
   }
-  return input;
 }
+
+export default nodeGlvrd;
--- test.js
+++ test.js
@@ -1,8 +1,12 @@
+'use strict';
+
 import test from 'ava';
 import nodeGlvrd from './index';
 
-test('should nodeGlvrd', (t) =>
-  t.is(nodeGlvrd('unicorns'), 'unicorns'));
+test.beforeEach(t => {
+  t.context.glvrd = new nodeGlvrd('testApp');
+});
+
+test('should save appName', t => t.is(t.context.glvrd.appName, 'testApp'));
 
 test('should throw on empty input', t => t.throws(() => { nodeGlvrd(); }, TypeError));
-test('should throw on invalid input', t => t.throws(() => { nodeGlvrd(2); }, TypeError));
