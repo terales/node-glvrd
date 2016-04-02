022757821a1275d031ba0c934c7ab1c3f759756c
TerAleS <www.terales.info>
2016-04-03 00:40:18 +0300

    :construction: :white_check_mark: Implemented testing without server interaction

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 'use strict';
 
 import test from 'ava';
+import sinon from 'sinon';
 import nodeGlvrd from './index';
 import endpointsSpec from './endpointsSpec.js';
 
 test.beforeEach(t => {
   t.context.glvrd = new nodeGlvrd('testApp');
+  t.context.requestStub = sinon.stub();
+
+  t.context.glvrd.req = t.context.requestStub;
 });
 
 test('should throw on empty appName', t => t.throws(() => { var t = new nodeGlvrd(); }, Error));
@@ -13,6 +17,8 @@ test('should throw on empty appName', t => t.throws(() => { var t = new nodeGlvr
 test('should save appName', t => t.is(t.context.glvrd.params.app, 'testApp'));
 
 test('should check server status', t => {
+  t.context.requestStub.yields(null, null, endpointsSpec.endpoints.getStatus.responseExample);
+
   return t.context.glvrd.checkStatus().then(
     response => t.same(response, endpointsSpec.endpoints.getStatus.responseExample)
   );
